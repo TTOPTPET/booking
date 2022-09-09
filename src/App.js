5ba42acb5e9a18e04963f7b513b9328e4cc9312a
forgetsmd <forgetsmd@gmail.com>
2022-09-09 14:13:04 +0300

    delete mothsArr

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import DatePicker from "./components/DatePicker/DatePicker";
 function App() {
   let currentDate = new Date();
 
-  const monthsArr = [
-    "январь",
-    "февраль",
-    "март",
-    "апрель",
-    "май",
-    "июнь",
-    "июль",
-    "август",
-    "сентябрь",
-    "октябрь",
-    "ноябрь",
-    "декабрь",
-  ];
-
   const [selectDateRange, setSelectDateRange] = useState(
     "12.02.2022 - 19.02.2022"
   );
@@ -30,18 +15,13 @@ function App() {
   });
   return (
     <div className="App">
-      <div>
-        {currentDate.toLocaleString("ru", {
-          month: "long",
-        })}
-      </div>
+      <div>{JSON.stringify(selectDate)}</div>
       <DatePicker
         selectDateRange={selectDateRange}
         setSelectDateRange={setSelectDateRange}
         currentDate={currentDate}
         selectDate={selectDate}
         setSelectDate={setSelectDate}
-        monthsArr={monthsArr}
       />
     </div>
   );
